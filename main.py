0c497149f2426a6bf82d8b321c5715fb8704b427
AJ-tech <43217154+abdulj40@users.noreply.github.com>
2019-03-08 21:30:23 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import kivy
+kivy.require('1.10.0')
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class CalcGridLayout(GridLayout):
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = "Error"
+
+
+class CalculatorApp(App):
+    def build(self):
+        return CalcGridLayout()
+
+
+CalcApp = CalculatorApp()
+CalcApp.run()
